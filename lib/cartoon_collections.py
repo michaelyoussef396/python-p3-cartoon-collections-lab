7f38c667811a97c20d9e629a2770b49aa2b4a2a0
michael youssef <michaelyoussef396@gmail.com>
2024-07-27 18:24:31 +1000

    completed tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,16 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f"{call.capitalize()}!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for food in foods:
+        if food in cheeses:
+            return food
+    return None
